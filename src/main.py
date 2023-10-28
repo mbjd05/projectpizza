343f6a2f6d9f3dd5c6702d40f5d6b7cb973b3fd2
M.B.J. Donners <532486@student.fontys.nl>
2023-10-28 16:46:45 +0200

    finished login functionality

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from flask_login import current_user, login_required
 from . import db
 
 main = Blueprint('main', __name__)
@@ -8,5 +9,6 @@ def index():
     return render_template('index.html')
 
 @main.route('/orders')
-def profile():
-    return render_template('orders')
+@login_required
+def orders():
+    return render_template('orders.html', username=current_user.name)
